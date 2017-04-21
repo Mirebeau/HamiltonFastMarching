0fbea9744369a1c5ae854029e85bf873598ff2bf
Mirebeau <jm.mirebeau@gmail.com>
2017-04-21 13:25:38 +0200

    removing deleted files from tracking

--- ExampleFiles/Matlab/RescaledCoords.m
+++
@@ -1,8 +0,0 @@
-function [NewCoords] = RescaledCoords(OldCoords,Origin,Spacing)
-    len = size(OldCoords); len=len(2);
-    Ones = ones(1,len);
-    Origins = Origin*Ones;
-    Spacings = Spacing*Ones;
-    
-    NewCoords = 1+(OldCoords-Origins)./Spacings;
-end
