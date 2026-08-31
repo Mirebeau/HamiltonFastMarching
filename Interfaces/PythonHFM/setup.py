420533c742d46f1d17145fa31b8167529be7182d
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2026-08-31 19:06:56 +0200

    Bump version, to include experimental models in build.

--- Interfaces/PythonHFM/setup.py
+++ Interfaces/PythonHFM/setup.py
@@ -69,7 +69,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name=PKG_NAME,
-    version='0.2.13',
+    version='0.2.13.post1',
     author='Jean-Marie Mirebeau',
     author_email='jm.mirebeau@gmail.com',
     description='HFM python wrapper',
